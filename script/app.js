c667e13c8ebaab9126dddd2860a0a4f572344249
LittleBlue418 <hollyathomas88@gmail.com>
2020-01-13 13:55:16 +0100

    remove task, clear list, filter added

--- script/app.js
+++ script/app.js
@@ -12,7 +12,17 @@ loadEventListeners();
 function loadEventListeners() {
   // Add task event
   form.addEventListener('submit', addTask);
+
+  // Remove task event
+  taskList.addEventListener('click', removeTask);
+
+  // Clear List
+  clearBtn.addEventListener('click', clearTasks);
+
+  // Filter through the tasks
+  filter.addEventListener('keyup', filterTasks);
 }
+
 
 // Add Task
 function addTask(e) {
@@ -41,6 +51,43 @@ function addTask(e) {
 
   // Clear input
   taskInput.value = '';
+}
 
 
+// Remove Task
+function removeTask(e) {
+  if(e.target.parentElement.classList.contains('delete-item')){
+    if(confirm('Are you sure?')) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+
+// Clear task list
+function clearTasks() {
+  // taskList.innerHTML = '';
+
+  // Faster
+  while(taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+
+
+// Filter Tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach((
+    function(task) {
+      const item = task.firstChild.textContent;
+
+      if(item.toLowerCase().indexOf(text) != -1){
+        task.style.display = 'block';
+      } else {
+        task.style.display = 'none';
+      }
+    }));
+      // we can run for each as querySelectorAll returns a node list
 }
